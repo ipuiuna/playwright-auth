927a36891a4734881006d8f15319b2b9d308efea
marcosdaniel <marcosdaniel@outlook.com>
2025-11-20 14:57:55 -0300

    all tests

---
+++ tests/auth.inventory.spec.ts
@@ -0,0 +1,22 @@
+import { expect } from '@playwright/test';
+import { test } from '../fixtures/loggedContext';
+import { InventoryPage } from '../pages/inventoryPage';
+
+test.describe('Tests with page authenticated', () => {
+  test(
+    'should be already logged in (auth setup working)',
+    { tag: '@auth' },
+    async ({ accessInventory }) => {
+      await expect(accessInventory).toHaveURL(/inventory.html/);
+    }
+  );
+
+  test(
+    'should display 6 items at /inventory.html',
+    { tag: '@auth' },
+    async ({ accessInventory }) => {
+      const inventoryPage = new InventoryPage(accessInventory);
+      await inventoryPage.assertInventoryIsVisible();
+    }
+  );
+});
