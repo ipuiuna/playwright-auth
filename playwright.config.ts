bc48a3c984649b935d99cc94822bea9cb99818cc
marcosdaniel <marcosdaniel@outlook.com>
2025-11-21 11:44:54 -0300

    make dotenv runnable only locally

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig, devices } from '@playwright/test';
 import * as dotenv from 'dotenv';
-dotenv.config();
+
+if (!process.env.CI) {
+  dotenv.config();
+}
 
 export default defineConfig({
   testDir: './tests',
