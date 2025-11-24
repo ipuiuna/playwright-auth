3f347e71d41be1a1fcd16e5cf3abb44cb7bab527
ipuiuna <marcosdaniel@outlook.com>
2025-11-24 19:48:37 -0300

    Refactor auth setup to remove unnecessary directory creation
    Removed directory creation for storage path in auth setup.

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,7 +1,6 @@
 import { test as setup } from '@playwright/test';
 import { LoginPage } from '../pages/loginPage';
 import path from 'path';
-import fs from 'fs';
 
 const STORAGE_PATH = path.join(__dirname, '../.auth/auth.json');
 
@@ -12,8 +11,6 @@ setup('Setup for authentication tests', async ({ page }) => {
     process.env.SAUCE_USERNAME!,
     process.env.SAUCE_PASSWORD!
   );
-
-  fs.mkdirSync(path.dirname(STORAGE_PATH), { recursive: true });
-
   await page.context().storageState({ path: STORAGE_PATH });
 });
+
